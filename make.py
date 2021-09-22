b0fe1dfd3ee27bef37cf7e9ba8d55ed18ae5e238
Tyler Trahan <tyler@tylertrahan.com>
2021-09-22 17:27:52 -0600

    Add: Oil Refinery

--- make.py
+++ make.py
@@ -9,7 +9,7 @@ newgrf_name = "lumberjack_industries"
 header_stuff = ["header", "cargos", "functions"]
 
 # Files to place in alphabetical order below
-unordered_stuff = ["logging_camp", "paper_mill", "farm", "bakery", "box_factory", "oil_rig"]
+unordered_stuff = ["logging_camp", "paper_mill", "farm", "bakery", "box_factory", "oil_rig", "oil_refinery"]
 
 # Do you want to copy the completed NewGRF to your OpenTTD folder? (True/False)
 copy_bool = True
